f003a388c3f2bc2c9728a78088043e0a38913657
Christian Fries <email@christian-fries.de>
2020-12-20 21:37:53 +0100

    Added conversion interface.

---
+++ src/main/java/net/finmath/aadexperiments/value/ConvertableToFloatingPoint.java
@@ -0,0 +1,12 @@
+package net.finmath.aadexperiments.value;
+
+public interface ConvertableToFloatingPoint {
+
+	/**
+	 * Returns the floating point value of this object
+	 *
+	 * @return Floating point value represented by this object
+	 */
+	Double asFloatingPoint();
+
+}
